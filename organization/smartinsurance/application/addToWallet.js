1ec655451487679a4caf5af02e2289c488317460
lucien <lokm13@gmail.com>
2019-03-07 16:14:24 +0800

    added back the identity

--- organization/smartinsurance/application/addToWallet.js
+++ organization/smartinsurance/application/addToWallet.js
@@ -12,7 +12,7 @@ const path = require('path');
 const fixtures = path.resolve(__dirname, '../../../basic-network');
 
 // A wallet stores a collection of identities
-const wallet = new FileSystemWallet('../identity/user/isabella/wallet');
+const wallet = new FileSystemWallet('../identity/user/tom/wallet');
 
 async function main() {
 
@@ -22,13 +22,12 @@ async function main() {
         // Identity to credentials to be stored in the wallet
         const credPath = path.join(fixtures, '/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com');
         const cert = fs.readFileSync(path.join(credPath, '/msp/signcerts/User1@org1.example.com-cert.pem')).toString();
-        const key = fs.readFileSync(path.join(credPath, '/msp/keystore/c75bd6911aca808941c3557ee7c97e90f3952e379497dc55eb903f31b50abc83_sk')).toString();
+        const key = fs.readFileSync(path.join(credPath, '/msp/keystore/cce404280f53be01aff1c12abb8d255c3acdbae690530de3ca2d8b0bd24361b4_sk')).toString();
 
         // Load credentials into wallet
         const identityLabel = 'User1@org1.example.com';
         const identity = X509WalletMixin.createIdentity('Org1MSP', cert, key);
-
-        await wallet.import(identityLabel, identity);
+await wallet.import(identityLabel, identity);
 
     } catch (error) {
         console.log(`Error adding to wallet. ${error}`);
