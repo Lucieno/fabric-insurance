1b01577f31dc30e808a903540388a9cf222fbc78
lucien <lokm13@gmail.com>
2019-03-07 20:52:26 +0800

    created a folder for police

---
+++ organization/police/application/addToWallet.js
@@ -0,0 +1,44 @@
+/*
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+// Bring key classes into scope, most importantly Fabric SDK network class
+const fs = require('fs');
+const { FileSystemWallet, X509WalletMixin } = require('fabric-network');
+const path = require('path');
+
+const fixtures = path.resolve(__dirname, '../../../basic-network');
+
+// A wallet stores a collection of identities
+const wallet = new FileSystemWallet('../identity/user/tom/wallet');
+
+async function main() {
+
+    // Main try/catch block
+    try {
+
+        // Identity to credentials to be stored in the wallet
+        const credPath = path.join(fixtures, '/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com');
+        const cert = fs.readFileSync(path.join(credPath, '/msp/signcerts/User1@org1.example.com-cert.pem')).toString();
+        const key = fs.readFileSync(path.join(credPath, '/msp/keystore/cce404280f53be01aff1c12abb8d255c3acdbae690530de3ca2d8b0bd24361b4_sk')).toString();
+
+        // Load credentials into wallet
+        const identityLabel = 'User1@org1.example.com';
+        const identity = X509WalletMixin.createIdentity('Org1MSP', cert, key);
+        await wallet.import(identityLabel, identity);
+
+    } catch (error) {
+        console.log(`Error adding to wallet. ${error}`);
+        console.log(error.stack);
+    }
+}
+
+main().then(() => {
+    console.log('done');
+}).catch((e) => {
+    console.log(e);
+    console.log(e.stack);
+    process.exit(-1);
+});
